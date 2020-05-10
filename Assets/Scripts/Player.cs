f978282c09a2d336aa5d990fbeffa31dd6ba4b6f
Fran Tena <tarod.net@gmail.com>
2020-05-10 11:35:36 +0200

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	Vector3 velocity;
+	
+	void Update () {
+		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * 10;
+	}
+
+	void FixedUpdate() {
+		GetComponent<Rigidbody>().MovePosition (GetComponent<Rigidbody>().position + velocity * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/Player2D.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2D : MonoBehaviour {
+	Vector2 velocity;	
+
+	void Update () {
+		velocity = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical")).normalized * 10;
+	}
+
+	void FixedUpdate() {
+		GetComponent<Rigidbody2D>().MovePosition (GetComponent<Rigidbody2D>().position + velocity * Time.fixedDeltaTime);
+	}
+
+    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            //... then set the other object we just collided with to inactive.
+            other.gameObject.SetActive(false);
+
+            //Add one to the current value of our count variable.
+            // count++;
+
+            //Update the currently displayed count by calling the SetCountText function.
+            // SetCountText();
+        }
+
+		Debug.Log (other.gameObject.name);
+    }
+
+	void OnCollisionEnter (Collision col)
+	{
+		Debug.Log (col.gameObject.name);
+	}
+
+	void OnCollisionStay (Collision col)
+	{
+		Debug.Log (col.gameObject.name);
+	}
+		
+}
